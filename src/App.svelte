451cf9628b0398cdc8bcccc637b230a749d5d84f
Teejay39 <63671611+Teejay39@users.noreply.github.com>
2023-01-15 14:15:34 +0100

    canvas function

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
-  import tutorialimg from "../src/assets/tutorial.png"
-  let type = "number";;
+  import canvasimg from "../src/assets/canvas.png";
+  let type = "number";
   let hoursPerDay = 1;
   let days = 1;
   const today = new Date();
@@ -19,6 +19,47 @@
   let costs = [prices[0] * quantity[0], prices[1] * quantity[1], prices[2] * quantity[2]]
   let costsPerHour = [costs[0] / (hoursPerDay * days), costs[1] / (hoursPerDay * days), costs[2] / (hoursPerDay * days)]
 
+  /*TJ's toller Canvas Code - start*/
+
+  let canvas = document.createElement('canvas');
+
+  canvas.id = "canv";
+  canvas.width = 562;
+  canvas.height = 98;
+  canvas.style.zIndex = "8";
+  canvas.style.position = "absolute";
+
+
+  let div = document.getElementById("canvas");
+  div.appendChild(canvas);
+
+  let ctx = canvas.getContext("2d");
+  const image = new Image();
+  image.src = canvasimg;
+  image.addEventListener(
+      "load",
+      () => {
+          ctx.drawImage(image, 0, 0);
+
+          ctx.font = '35pt calibri';
+          ctx.textAlign = 'left';
+          ctx.textBaseline = 'top';
+          ctx.fillStyle = '#ffeac2';
+          ctx.shadowBlur = 20;
+          ctx.fillText(String(level), 150, 5);
+
+          ctx.font = '25pt calibri';
+          ctx.textAlign = 'right';
+          ctx.textBaseline = 'top';
+          ctx.fillStyle = '#fff';
+          ctx.shadowBlur = 20;
+          ctx.fillText(String(level), 420, 15);
+      },
+      false
+  );
+
+  /*TJ's toller Canvas Code - end*/
+
   function calculate() {
     quantity = [Math.ceil(hoursPerDay/3)*days, days, Math.ceil(days/3)];
     costs = [prices[0] * quantity[0], prices[1] * quantity[1], prices[2] * quantity[2]]
@@ -28,6 +69,33 @@
     collectedPoints = (level - 1) * 1000 + points;
     remainingPoints = maxPoints - collectedPoints;
     dailyGoal = Math.ceil(remainingPoints / daysLeft);
+
+    let ctx = canvas.getContext("2d");
+    const image = new Image();
+    image.src = canvasimg;
+    image.addEventListener(
+        "load",
+        () => {
+            ctx.drawImage(image, 0, 0);
+
+            ctx.font = '35pt calibri';
+            ctx.textAlign = 'left';
+            ctx.textBaseline = 'top';
+            ctx.fillStyle = '#ffeac2';
+            ctx.shadowColor = "black";
+            ctx.shadowBlur = 20;
+            ctx.fillText(String(level), 150, 5);
+
+            ctx.font = '25pt calibri';
+            ctx.textAlign = 'right';
+            ctx.textBaseline = 'top';
+            ctx.fillStyle = '#fff';
+            ctx.shadowColor = "black";
+            ctx.shadowBlur = 20;
+            ctx.fillText(String(points), 420, 15);
+        },
+        false
+    );
   }
 </script>
 
@@ -37,7 +105,7 @@
   <h2>Event Progress Calculator</h2>
   <h3>Calculate your daily goal for points in order to finish the event</h3>
 </hgroup>
-<img alt="tutorialimg" src={tutorialimg}>
+<div id="canv" style="width: 562px; heigth: 98px;"></div>
 <h3>What level is your next?</h3>
 <input bind:value={level} on:input={calculateDailyGoal} type="number" min="1" max="50">
 <h3>How many points have you already collected to unlock level {level}?</h3>
